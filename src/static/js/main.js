ad3e7ebdb7aacea878a8c8ac815d189e27322aad
lukas.vedral <lukas.vedral.023@pslib.cz>
2026-06-07 21:34:30 +0200

    m

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,48 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const swiperDocuments = new Swiper('#slider-documents', {
-        slidesPerView: 1,
-        loop: document.querySelectorAll(' #slider-documents .swiper-slide').length >= 6,
-        watchOverflow: false,
-
-        breakpoints: {
-            600: {
-            slidesPerView: 2,
-            spaceBetween: 48,
-            },
-            960: {
-            slidesPerView: 3,
-            spaceBetween: 64,
-            },
+const swiperDocuments = new Swiper('#slider-documents', {
+    slidesPerView: 1,
+    spaceBetween: 32,
+    watchOverflow: false,
+    loop: false,
+    breakpoints: {
+        600: {
+        slidesPerView: 2,
+        spaceBetween: 48,
         },
+        960: {
+        slidesPerView: 3,
+        spaceBetween: 64,
+        },
+    },
 
 
-        pagination: {
-        el: '#slider-documents .swiper-pagination',
-        clickable: true,
-        },
-        navigation: {
-        nextEl: '#slider-documents .swiper-button-next',
-        prevEl: '#slider-documents .swiper-button-prev',
-        },
-    });
+    pagination: {
+    el: '#slider-documents .swiper-pagination',
+    clickable: true,
+    },
+    navigation: {
+    nextEl: '#slider-documents .swiper-button-next',
+    prevEl: '#slider-documents .swiper-button-prev',
+    },
+});
 
   const swiperTeam = new Swiper('#slider-team', {
     slidesPerView: 1,
-    loop: document.querySelectorAll(' #slider-team .swiper-slide').length >= 6,
     watchOverflow: false,
-            breakpoints: {
-            600: {
-            slidesPerView: 2,
-            spaceBetween: 48,
-            },
-            960: {
-            slidesPerView: 3,
-            spaceBetween: 64,
-            },
+    spaceBetween: 32,
+    loop: false,
+    breakpoints: {
+        600: {
+        slidesPerView: 2,
+        spaceBetween: 48,
         },
-    pagination: {
-      el: '#slider-team .swiper-pagination',
-      clickable: true,
+        960: {
+        slidesPerView: 3,
+        spaceBetween: 64,
+        },
     },
     navigation: {
       nextEl: '#slider-team .swiper-button-next',
